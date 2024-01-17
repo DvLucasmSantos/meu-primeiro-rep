ba1d1ccf69c2a2fcdc96a8866f93e94b2e7e8c37
LucasM-Santos <lucasmsantos.pessoal@gmail.com>
2024-01-17 15:48:41 -0300

    exercicios

---
+++ exercicio-1.js
@@ -0,0 +1,46 @@
+const cidadeNatal = "Serra Talhada";
+
+console.log(cidadeNatal);
+
+let populacao = 12_000_000;
+
+console.log(populacao);
+
+populacao = 12_320_000;
+
+console.log(populacao)
+
+let x = 10 , y = 2;
+let resultado = x + y;
+let mensagem = "A soma de " + x + " e " + y + " e igual a " + resultado;
+
+console.log(mensagem);
+
+resultado = x - y;
+mensagem ="A subtracao de " + x + " e " + y + " e igual a " + resultado;
+
+console.log(mensagem);
+
+resultado = x * y;
+mensagem ="A mutiplicacao de " + x + " e " + y + " e igual a " + resultado;
+
+console.log(mensagem);
+
+resultado = x / y;
+mensagem ="A divisao de " + x + " e " + y + " e igual a " + resultado;
+
+console.log(mensagem);
+
+let a = 10 , b = 2;
+let calculo = x + y;
+
+console.log(`A soma de ${a} e ${b} e igual a ${resultado}`);
+
+resultado = a - b;
+console.log(`A subtracao de ${a} e ${b} e igual a ${resultado}`);
+
+resultado = a * b;
+console.log(`A multiplicacao de ${a} e ${b} e igual a ${resultado}`);
+
+resultado = a / b;
+console.log(`A divisao de ${a} e ${b} e igual a ${resultado}`);
